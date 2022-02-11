c90f32b95348c117a68d7a1774864715e52990f4
joalvarado2 <jonathanalva92@gmail.com>
2022-02-10 21:32:15 -0500

    separando componentes y consumiendo api para desplegar lista de paises en una tabla

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,10 +9,25 @@ import { Component } from '@angular/core';
 })
 export class PorPaisComponent {
   termino: string = 'Hola mundo';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
-  constructor() {}
+  constructor(private paisService: PaisService) {}
 
-  buscar() {
+  buscar(termino : string) {
+    this.hayError = false;
+    this.termino = termino;
     console.log(this.termino);
+
+    this.paisService.buscarPais(this.termino).subscribe(
+      (paises) => {
+        console.log(paises);
+        this.paises = paises;
+      },
+      (err) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
   }
 }
